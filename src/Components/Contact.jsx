cd9969574c63cfbf26e12fdd25eb297020d0d82a
Rohit Bhat <88024795+RohithBhat19@users.noreply.github.com>
2023-12-17 08:59:10 +0530

    Add files via upload

---
+++ src/Components/Contact.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Contact = () => {
+  return (
+    <div className='contact'><marquee behavior="alternate" direction="right">Contact Us</marquee></div>
+  )
+}
+
+export default Contact
